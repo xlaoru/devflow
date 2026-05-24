90a71897526626934a682392c5030ad38eadcefb
xlaoru <sia212007@gmail.com>
2026-05-24 22:57:35 +0300

    Basic config

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-3xl text-violet-700 font-black">
+        Welcome to Next.js 👋
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
