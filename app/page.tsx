95a7aaea3122a1dd0dcf9b7737abeb465446ecdf
xlaoru <sia212007@gmail.com>
2026-05-28 12:58:45 +0300

    Setup assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 const Home = () => {
   return (
     <div>
-      <h1 className="text-3xl text-violet-700 font-black">
-        Welcome to Next.js 👋
-      </h1>
+      <h1 className="h1-bold">Welcome to Next.js 👋</h1>
     </div>
   );
 };
